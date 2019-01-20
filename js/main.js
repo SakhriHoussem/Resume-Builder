f64cdfd0178aff940536e91ee03f701d73c5284b
SakhriHoussem <s.houssem.e@gmail.com>
2019-01-20 20:54:27 +0100

    cv-01: submit the forms - name - job - email - phone - resume

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-$(document).foundation()
+$(document).foundation();
+window.onload = function () {
+    // events ----------------------------------
+    write("name");
+    write("job");
+    write("phone");
+    write("email");
+    write("resume");
+};
+
+// get elements ------------------------------
+
+// functions ----------------------------------
+function write($inputID) {
+    let input = document.getElementById($inputID);
+    input.addEventListener("blur", () => {
+        let $to = document.getElementById('cv_'+$inputID);
+        $to.textContent = input.value;
+    });
+}
+
+
