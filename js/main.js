4a688bceb5244dbc258dc5ae19c67a0dbcae2c62
SakhriHoussem <s.houssem.e@gmail.com>
2019-01-21 15:51:51 +0100

    cv-01 : submit link form

--- js/main.js
+++ js/main.js
@@ -1,22 +1,56 @@
 $(document).foundation();
-window.onload = function () {
-    // events ----------------------------------
-    write("name");
-    write("job");
-    write("phone");
-    write("email");
-    write("resume");
-};
 
 // get elements ------------------------------
 
 // functions ----------------------------------
-function write($inputID) {
-    let input = document.getElementById($inputID);
-    input.addEventListener("blur", () => {
-        let $to = document.getElementById('cv_'+$inputID);
-        $to.textContent = input.value;
+function addInfo(inputName) {
+    let input = document.getElementById("input-" + inputName);
+    input.addEventListener("keypress", ($event) => {
+        let to = document.getElementById('cv__' + inputName);
+        to.textContent = $event.target.value;
+    });
+
+    input.addEventListener("blur", ($event) => {
+        let to = document.getElementById('cv__' + inputName);
+        to.textContent = $event.target.value;
     });
 }
+function addWebSite(inputName) {
+    // get elements
+    let input = document.getElementById("input-" + inputName);
+    let button = document.getElementById("button-" + inputName + "_add");
+
+    button.addEventListener("click", ($event) => {
+        $event.preventDefault();
+        let divTag = document.createElement("div");
+
+        // create <i class="fas fa-globe-europe"></i>
+        let iTag = document.createElement("i");
+        iTag.classList = "fas fa-globe-europe";
+
+        // create link tag
+        let linkTag = document.createElement("a");
+        let linkText = document.createTextNode(input.value);
+
+        linkTag.appendChild(linkText);
+        linkTag.title = input.value;
+        linkTag.href = input.value;
+
+        // get the element to write inside
+        let to = document.getElementById('cv__' + inputName);
+        divTag.appendChild(iTag);
+        divTag.appendChild(linkTag);
+        to.appendChild(divTag);
+    });
+}
+
+// events ----------------------------------
+addInfo("name");
+addInfo("job");
+addInfo("phone");
+addInfo("email");
+addInfo("resume");
+addWebSite("web-sites");
+
 
 
